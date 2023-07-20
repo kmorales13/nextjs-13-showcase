bf99840fc930811b2b0de18c3f546df12ce8edd4
kmorales13 <equinoxstudiosteam@gmail.com>
2023-07-20 00:20:08 -0600

    fix null category

--- lib/actions.ts
+++ lib/actions.ts
@@ -44,7 +44,7 @@ const makeGraphQLRequest = async (query: string, variables = {}) => {
 export const fetchAllProjects = (category?: string | null, endcursor?: string | null) => {
   client.setHeader("x-api-key", apiKey);
 
-  return makeGraphQLRequest(projectsQuery, { category, endcursor });
+  return makeGraphQLRequest(projectsQuery, { category: category ?? '', endcursor });
 };
 
 export const createNewProject = async (form: ProjectForm, creatorId: string, token: string) => {
